07000a4fcff6991ddff49d810ad2e94fb50869f1
Wesley Alves <wesleyapelao06@gmail.com>
2022-11-23 19:26:37 -0400

    Deploy for firebase

--- public/mobile.js
+++ public/mobile.js
@@ -2,6 +2,17 @@ const doc = document;
 const menuOpen = doc.querySelector(".menu");
 const menuClose = doc.querySelector(".close");
 const overlay = doc.querySelector(".overlay");
+const option = doc.querySelectorAll(".option-menu");
+
+
+
+option.forEach(item => {
+  item.addEventListener("click", () => {
+    overlay.classList.remove("overlay--active");
+  });
+});
+
+
 
 menuOpen.addEventListener("click", () => {
   overlay.classList.add("overlay--active");
